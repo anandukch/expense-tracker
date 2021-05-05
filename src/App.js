6e2acd040fa08478005c144665750c15974df75e
Anandu-kichu <anandukch@gmail.com>
2021-05-05 16:16:25 +0530

    dummy data removed

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import NewExpense from './components/NewExpense/NewExpense';
 import Expense from './components/expenses/Expense'
 
 const DUMMY = [
-  { id: "e1", title: "airpod", amount: "500", date: new Date(2020, 3, 21) },
-  { id: "e2", title: "car", amount: "2000", date: new Date(2020, 2, 25) },
-  { id: "e3", title: "bike", amount: "200", date: new Date(2020, 4, 31) },
-  { id: "e4", title: "phone", amount: "20", date: new Date(2022, 3, 21) },
+  // { id: "e1", title: "airpod", amount: "500", date: new Date(2020, 3, 21) },
+  // { id: "e2", title: "car", amount: "2000", date: new Date(2020, 2, 25) },
+  // { id: "e3", title: "bike", amount: "200", date: new Date(2020, 4, 31) },
+  // { id: "e4", title: "phone", amount: "20", date: new Date(2022, 3, 21) },
 ];
 
 const App=() => {
